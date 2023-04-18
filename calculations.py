7982342c49e580d2790f5ab5ceeb419bd0ac16a1
namansm <namansm@umich.edu>
2023-04-18 04:44:18 -0400

    calculations

--- calculations.py
+++ calculations.py
@@ -15,9 +15,26 @@ def open_database(db_name):
 
 
 def calc_and_write_averages(outfile, cur, con):
-    cur.execute("SELECT c.name, w.temperature, w.wind, w.rain FROM weather w INNER JOIN Countries c ON w.country_id = c.id")
-    data = cur.fetchall()
-    print(data)
+    cur.execute("SELECT c.name, w.month, AVG(w.temperature), AVG(w.wind), AVG(w.rain) FROM weather w INNER JOIN Countries c ON w.country_id = c.id WHERE w.country_id=1 GROUP BY w.month")
+    ind_data = cur.fetchall() 
+    cur.execute("SELECT c.name, w.month, AVG(w.temperature), AVG(w.wind), AVG(w.rain) FROM weather w INNER JOIN Countries c ON w.country_id = c.id WHERE w.country_id=2 GROUP BY w.month")
+    sui_data = cur.fetchall() 
+    
+    months = ["xx", "xx", "xx", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February", "March"]
+
+    outFile = open(outfile, "w")
+    csvOut = csv.writer(outFile)
+
+    csvOut.writerow(("Country","Month","Average Daily Temperature (C)","Average Daily Wind Speed (kph)", "Average Daily Rainfall (mm)"))
+
+    data = []
+    data.extend(ind_data)
+    data.extend(sui_data)
+    for t in data:
+        csvOut.writerow(t)
+
+    return data
+
 
 
 def plot_india_tempvscases(db):
@@ -38,11 +55,9 @@ def plot_india_tempvscases(db):
     plt.ylabel('Number of Cases')
     plt.savefig('india_temp_vs_cases.png')
 
-
 def main():
-    cur, conn = open_database("covid_weather.db")
-    calc_and_write_averages("output_data.csv", cur, conn)
-
+     cur, conn = open_database('covid_weather.db')
+     data = calc_and_write_averages("output.csv", cur, conn)
 
 if __name__ == '__main__':
     main()
--- weather_api.py
+++ weather_api.py
@@ -35,12 +35,12 @@ def get_covid_data(cur, conn):
 
 # Data should be a list of dictionaries
 def load_weather_data(data, cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS weather (case_id INTEGER PRIMARY KEY, country_id INTEGER, temperature REAL, wind REAL, rain REAL)")
+    cur.execute("CREATE TABLE IF NOT EXISTS weather (case_id INTEGER PRIMARY KEY, country_id INTEGER, month INTEGER, temperature REAL, wind REAL, rain REAL)")
 
     url = "https://meteostat.p.rapidapi.com/point/daily"
 
     headers = {
-        "X-RapidAPI-Key": "09dff8466dmsh2dcda9bcb212375p1bc56ejsn43f15c65bb68",
+        "X-RapidAPI-Key": "72117917afmsh2f97c3d988f6f27p1c320ajsn11aee594d295",
         "X-RapidAPI-Host": "meteostat.p.rapidapi.com"
     }
 
@@ -61,14 +61,20 @@ def load_weather_data(data, cur, conn):
     response = requests.get(url, headers=headers, params=params)
     res = response.json()["data"]
     for i in range(0, len(res)):
-        locs[i] = (1, res[i])
+        mon = int(res[i]["date"][5:7])
+        if(res[i]["date"][0:4] == "2021"):
+            mon = mon + 12
+        locs[i] = (1, res[i], mon)
     l = len(res)
 
     params = {"lat":sui_locs[0],"lon":sui_locs[1],"start":"2020-03-01","end":"2021-03-31"}
     response = requests.get(url, headers=headers, params=params)
     res = response.json()["data"]
     for i in range(0, len(res)):
-        locs[i+l] = (2, res[i])
+        mon = int(res[i]["date"][5:7])
+        if(res[i]["date"][0:4] == "2021"):
+            mon = mon + 12
+        locs[i+l] = (2, res[i], mon)
     # END SECTION
 
     while count < 25 and ind < len(locs):
@@ -81,11 +87,11 @@ def load_weather_data(data, cur, conn):
         # response = requests.get(url, headers=headers, params=params)
         # weather = response.json()["data"]
 
-        cur.execute("INSERT OR IGNORE INTO weather (case_id, country_id, temperature, wind, rain) VALUES (?,?,?,?,?)", (ind, d[0], d[1]["tavg"], d[1]["wspd"], d[1]["prcp"],))
+        cur.execute("INSERT OR IGNORE INTO weather (case_id, country_id, month, temperature, wind, rain) VALUES (?,?,?,?,?,?)", (ind, d[0], d[2], d[1]["tavg"], d[1]["wspd"], d[1]["prcp"],))
 
         cur.execute("SELECT * FROM weather")
         new_len = len(cur.fetchall())
-        
+
         if prev_len == new_len:
             
             ind += 1
